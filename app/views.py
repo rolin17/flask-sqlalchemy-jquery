e56e30c5bf2cdcd7419ab5a217673e541448ae8e
rolin17 <juatrolin@yahoo.com.ph>
2016-06-09 15:20:56 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -65,3 +65,12 @@ def delete(id):
 	activities  = Task.query.all()
 	count 		= Task.query.count()
 	return render_template('index.html', activities=activities, count=count)
+	
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
